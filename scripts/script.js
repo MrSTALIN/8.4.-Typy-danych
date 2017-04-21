f546de0b8187dbff7fd0644bdfcb126c419fd0f8
Maciek <zdenekj23@o2.pl>
2017-04-21 23:50:23 +0200

    Pole trojkata

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+var a = prompt("Please insert A value");
+var h = prompt("Please insert H value");
+var triangleArea = a*h/2;
+
+alert("Triangle area is equal " + triangleArea + "\nA= " + a + "\nH= " + h);
